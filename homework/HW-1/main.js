379993b38116c845d60a85c7732ba0e2ff1921a3
miami-coder <defenderxz229@gmail.com>
2025-10-13 20:21:49 +0300

    first commit

---
+++ homework/HW-1/main.js
@@ -0,0 +1,53 @@
+// task 1
+// let a = 'hello';
+// console.log(a);
+// let b = 'owu';
+// console.log(b);
+// let c = 'com';
+// console.log(c);
+// let d = 'ua';
+// console.log(d);
+// let e = 1;
+// console.log(e);
+// let f = 10;
+// console.log(f);
+// let g = -999;
+// console.log(g);
+// let h = 123;
+// console.log(h);
+// let i = 3.14;
+// console.log(i);
+// let j = 2.7;
+// console.log(j);
+// let k = 16;
+// console.log(k);
+// let l = true;
+// console.log(l);
+// let m = false;
+// console.log(m);
+
+
+// task 2
+// let firstName = 'Bohdan';
+// let middleName = 'Olegovich';
+// let lastName = 'Palamarchuk';
+// let person = `${firstName} ${middleName} ${lastName}`;
+// console.log(person);
+
+
+//task 3
+// let a = 100;
+// console.log(typeof a);
+// let b = '100';
+// console.log(typeof b);
+// let c = true;
+// console.log(typeof c);
+
+
+//task 4
+// let a = prompt('Enter your first name');
+// let b = prompt('Enter your last name');
+// let c = prompt('Enter your age');
+// console.log(a);
+// console.log(b);
+// console.log(c);
